a3d332a8ee6d4a5479904bedeb041f71af29f719
Your Name <you@example.com>
2018-07-09 18:51:42 +0530

    modified to add status

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { } from '@types/googlemaps';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-map',
@@ -13,124 +14,76 @@ export class MapComponent {
 
   isTracking = false;
 
-  marker: google.maps.Marker;
+  currentLat: any;
+  currentLong: any;
 
-  ngOnOldInit() {
+  marker: google.maps.Marker;
+  marker1: google.maps.Marker;
+
+
+  ngOnInit() {
     var markerArray = [];
 
-  // Instantiate a directions service.
-  var directionsService = new google.maps.DirectionsService;
+    // Instantiate a directions service.
+    var directionsService = new google.maps.DirectionsService;
 
-  // Create a map and center it on Manhattan.
-  var map = new google.maps.Map(this.gmapElement.nativeElement, {
-    zoom: 11,
-    center: {lat: 18.52903833, lng: 73.86974667}
-  });
-
-  // Create a renderer for directions and bind it to the map.
-  var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
-
-  // Instantiate an info window to hold step text.
-  var stepDisplay = new google.maps.InfoWindow;
-  }
-
-/////////////////////////////////////
-ngOnInit() {
-  var markerArray = [];
-
-  // Instantiate a directions service.
-  var directionsService = new google.maps.DirectionsService;
-
-  // Create a map and center it on Manhattan.
-  var map = new google.maps.Map(this.gmapElement.nativeElement, {
-    zoom: 13,
-    center: {lat: 40.771, lng: -73.974}
-  });
-
-  // Create a renderer for directions and bind it to the map.
-  var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
-
-  // Instantiate an info window to hold step text.
-  var stepDisplay = new google.maps.InfoWindow;
-
-  // Display the route between the initial start and end selections.
-  this.calculateAndDisplayRoute(
-      directionsDisplay, directionsService, markerArray, stepDisplay, map);
-  // Listen to change events from the start and end lists.
-  var onChangeHandler = function() {
-    this.calculateAndDisplayRoute(
-        directionsDisplay, directionsService, markerArray, stepDisplay, map);
-  };
-  
-}
-
-calculateAndDisplayRoute(directionsDisplay, directionsService,
-    markerArray, stepDisplay, map) {
-  // First, remove any existing markers from the map.
-  for (var i = 0; i < markerArray.length; i++) {
-    markerArray[i].setMap(null);
-  }
-
-  // Retrieve the start and end locations and create a DirectionsRequest using
-  // WALKING directions.
-  directionsService.route({
-    origin: "Vadgaon Mawal",
-    destination: "Talegaon",
-    travelMode: 'TRANSIT',
-    transitOptions: {
-  		departureTime: new Date(Date.now()),
-  		modes: ["TRAIN"]
-		}
-  }, function(response, status) {
-    // Route the directions and pass the response to a function to create
-    // markers for each step.
-    if (status === 'OK') {
-      directionsDisplay.setDirections(response);
-      this.showSteps(response, markerArray, stepDisplay, map);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-}
-
-showSteps(directionResult, markerArray, stepDisplay, map) {
-  // For each step, place a marker, and add the text to the marker's infowindow.
-  // Also attach the marker to an array so we can keep track of it and remove it
-  // when calculating new routes.
-  var myRoute = directionResult.routes[0].legs[0];
-  for (var i = 0; i < myRoute.steps.length; i++) {
-    var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
-    marker.setMap(map);
-    marker.setPosition(myRoute.steps[i].start_location);
-    this.attachInstructionText(
-        stepDisplay, marker, myRoute.steps[i].instructions, map);
-  }
-}
-
-
-attachInstructionText(stepDisplay, marker, text, map) {
-  google.maps.event.addListener(marker, 'click', function() {
-    // Open an info window when the marker is clicked on, containing the text
-    // of the step.
-    stepDisplay.setContent(text);
-    stepDisplay.open(map, marker);
-  });
-}
-
-showTrackingPosition(position) {
-  let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  this.map.panTo(location);
-
-  if (!this.marker) {
-    this.marker = new google.maps.Marker({
-      position: location,
-      map: this.map,
-      title: 'Got you!'
+    // Create a map and center it on Manhattan.
+    var map = new google.maps.Map(this.gmapElement.nativeElement, {
+      zoom: 17,
+      center: new google.maps.LatLng(18.5793, 73.8143)
     });
-  }
-  else {
-    this.marker.setPosition(location);
-  }
-}
 
+    // Create a renderer for directions and bind it to the map.
+    var directionsDisplay = new google.maps.DirectionsRenderer({ map: map });
+
+    // Instantiate an info window to hold step text.
+    var stepDisplay = new google.maps.InfoWindow;
+
+    this.currentLat = 18.528845;
+    this.currentLong = 73.87177667;
+    var origin = new google.maps.LatLng(this.currentLat, this.currentLong)
+    var destination = new google.maps.LatLng(18.52884167, 73.86566167)
+   
+    var image = {
+      url: "assets/train-icon.png",
+      size: new google.maps.Size(40, 45)
+    };
+    var mark = new google.maps.Marker({
+    position: origin,
+    map: map,
+    title: 'Train Coordinates',
+    icon : image
+  });
+
+  var mark1 = new google.maps.Marker({
+    position: destination,
+    map: map,
+    title: 'Train Coordinates',
+    icon : image
+  });
+
+    Observable.interval(1000).subscribe(x => {
+    
+      var location1 = { lat: this.currentLat, long: this.currentLong };
+      this.trackTrains(location1, map , mark1);
+      this.currentLat = (this.currentLat + 0.000020);
+      this.currentLong = (this.currentLong + 0.00052);
+
+      var location = { lat: this.currentLat, long: this.currentLong };
+      origin = new google.maps.LatLng(this.currentLat, this.currentLong)
+      this.trackTrains(location, map , mark);
+     
+    });
+
+  }
+
+  trackTrains(trainloc, map , marker) {
+    let location = new google.maps.LatLng(trainloc.lat, trainloc.long);
+    map.panTo(location);
+    var image = {
+      url: "assets/train-icon.png",
+      size: new google.maps.Size(100, 100)
+    };
+    marker.setPosition(location);
+  }
 }
